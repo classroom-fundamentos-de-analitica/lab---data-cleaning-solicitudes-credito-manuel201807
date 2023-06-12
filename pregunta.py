a8d8fe608ffd41a892b65932fae39ab326eb0b90
manuel201807 <mamejiaf@unal.edu.co>
2023-06-12 09:44:49 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -2,24 +2,20 @@
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
 
-
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada
+Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
-
 
 """
 import re
 import pandas as pd
 from datetime import datetime
 
-
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
-   
+    
     # Eliminación de datos faltantes y duplicados
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
-
 
     # Poner en minúscula las columnas de tipo texto y eliminar carácteres especiales de idea_negocio y barrio
     for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
@@ -27,21 +23,19 @@ def clean_data():
         df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
         df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
 
-
     # Depurar la columna monto_del_credito quitando los carácteres extraños (No numéricos)
     df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
     df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
     df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
     df['monto_del_credito'] = df['monto_del_credito'].astype(int)
-   
+    
     # Corregir tipo de dato de la columna comuna_ciudadano
     df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
-
 
     # Corregir tipo de dato de la columna comuna_ciudadano -> Pasar a formato de fecha
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
 
-
     # Luego de depurar, se pueden haber generado duplicados, por lo que se eliminan de nuevo
     df.drop_duplicates(inplace = True)
+
     return df
